15f11aa3ad8908b824ccc0847db04907a8759925
Uzair Hashmani <uz.hashmani@gmail.com>
2022-08-01 04:21:10 +0500

    Create myscripts.js

---
+++ Js101byF/002/myscripts.js
@@ -0,0 +1,4 @@
+document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i>"
+function myResponse() {
+  document.getElementById("message").innerHTML = "It's not you, it's me!";
+}
