cd5c139cc47ecbac65cb09419221983dde90c3f1
Uzair Hashmani <uz.hashmani@gmail.com>
2022-08-01 06:02:48 +0500

    Create myscripts.js

---
+++ Js101byF/003/myscripts.js
@@ -0,0 +1,35 @@
+
+
+/*
+These lines were causing problem in github pages
+
+document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i>"
+function myResponse() {
+  document.getElementById("message").innerHTML = "It's not you, it's me!";
+}
+
+so i used onload function 
+refference
+
+https://stackoverflow.com/questions/18239430/cannot-set-property-innerhtml-of-null
+
+
+*/
+
+
+window.onload = function() {
+        what();
+        function what() {
+document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i>"
+        };
+
+    }
+
+
+
+function myResponse() {
+  document.getElementById("message").innerHTML = "It's not you, it's me!";
+}
+
+
+window.alert("Hello alert test")
