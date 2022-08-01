7feb19fc397dda9a6fb2727aec36a4d421e09e07
Uzair Hashmani <uz.hashmani@gmail.com>
2022-08-01 05:02:40 +0500

    Update myscripts.js

--- Js101byF/002/myscripts.js
+++ Js101byF/002/myscripts.js
@@ -2,3 +2,14 @@ document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i
 function myResponse() {
   document.getElementById("message").innerHTML = "It's not you, it's me!";
 }
+
+
+window.onload = function() {
+document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i>"
+        what();
+
+        function myResponce() {
+ document.getElementById("message").innerHTML = "It's not you, it's me!";"
+        };
+
+    }
