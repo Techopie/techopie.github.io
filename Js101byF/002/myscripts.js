a28cd9964908e5df583e88e97aa40d1ddbb94ff5
Uzair Hashmani <uz.hashmani@gmail.com>
2022-08-01 05:36:34 +0500

    Update myscripts.js

--- Js101byF/002/myscripts.js
+++ Js101byF/002/myscripts.js
@@ -28,9 +28,5 @@ document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i
     }
 
 function myResponse() {
-  document.getElementById("message").innerHTML = "It's not you, it's me!
-
-  <div style="text-aling: center; margin: auto"><iframe src="http://www.youtube.com/embed/33YPOr6ZGw4?yt:stretch=16:9&vq=hd720p&loop=0&color=red&iv_load_policy=3&rel=0&showinfo=0&autohide=0&controls=1" width="560" height="315" allowtransparency="true" frameborder="0" ></iframe></div>
-  
-  ";
+  document.getElementById("message").innerHTML = "It's not you, it's me";
 }
