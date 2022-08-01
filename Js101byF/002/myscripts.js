5b5755a3dc06333720e37ecf5c7cbb1e04de3fd1
Uzair Hashmani <uz.hashmani@gmail.com>
2022-08-01 05:08:55 +0500

    Update myscripts.js

--- Js101byF/002/myscripts.js
+++ Js101byF/002/myscripts.js
@@ -1,7 +1,22 @@
+
+
+/*
+These lines were causing problem in github pages
+
 document.getElementById("message").innerHTML = "Hello <b>World</b> <i>testing</i>"
 function myResponse() {
   document.getElementById("message").innerHTML = "It's not you, it's me!";
 }
+
+
+refference
+
+https://stackoverflow.com/questions/18239430/cannot-set-property-innerhtml-of-null
+
+
+*/
+
+
 
 
 window.onload = function() {
